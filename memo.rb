462dde821a0a3ef03a0d592123b2c77174f70337
hikotomo <tomo.k9090@gmail.com>
2022-05-07 10:07:16 +0900

    files modify

--- memo.rb
+++ memo.rb
@@ -100,3 +100,139 @@ unless n.zero?
 else 
   puts "This is zero."
 end
+
+puts "\nテスト12"
+stone = "peridot"
+if stone == "ruby"
+  puts "July"
+elsif stone == "peridot"
+  puts "August"
+elsif stone == "sapphire"
+  puts "September"
+else 
+  puts "Not Found"
+end
+
+puts "\nテスト13"
+stone = "ruby"
+case stone
+when "ruby"
+  puts "July"
+when "peridot"
+    puts "August"
+when "sapphire"
+    puts "September"
+else
+  puts "Not Found"
+end
+
+puts "\nテスト14"
+def hello_world
+  puts "Hello, World!"
+end
+
+hello_world
+
+puts "\nテスト15"
+def add(x, y)
+  x + y # 最後に評価されたものが戻り値になる
+end
+
+puts add(2, 5)
+
+puts "\nテスト16"
+def cry(animal)
+  if animal == "cat"
+    "meow"
+  elsif animal >= "dog"
+    "bowwow"
+  elsif animal >= "cow"
+    "moomoo"
+  else
+    "Not found."
+  end
+end
+
+puts cry("cat")
+
+puts "\nテスト17"
+def fizz_buzz(x)
+  if x % 15 == 0
+    "Fizz"
+  elsif x % 5 == 0
+    "Buzz"
+  elsif x % 3 == 0
+    "FizzBuzz"
+  else
+    x.to_s
+  end
+end
+
+puts fizz_buzz(3)
+puts fizz_buzz(5)
+puts fizz_buzz(15)
+
+puts "\nテスト18"
+print "改行されない出力"
+p "そのまま戻り値となる → デバック用のメソッド"
+pp "pメソッドより見やすくなる"
+
+puts "\nテスト19"
+e = [1, "a", [1, 2, 3]]
+f = []
+puts e
+puts e.empty?
+p f.empty?
+
+puts "\nテスト20"
+puts e.include?("a") # 含んでいるか調べる
+p e.reverse # ひっくり返る
+p e
+p e.reverse! # 破壊的メソッド
+p e
+
+puts "\nテスト21"
+p e.shuffle # 配列がシャッフルされる
+
+puts "\nテスト22"
+p (0..25).to_a # 0~25をarrayにして！という意味
+p (0..25).to_a.shuffle!
+
+puts "\nテスト23"
+g = (0..10).to_a
+p g << 20 # 配列のけつに挿入 
+p g # しかも上は破壊的メソッド
+
+puts "\nテスト24"
+p g.push(30) # これもけつに挿入
+p g # 同じく破壊的メソッド
+
+puts "\nテスト25"
+# pop shiftは逆
+p g.pop
+p g
+p g.shift 
+p g
+
+puts "\nテスト26"
+g << 3
+p g
+p g.uniq #重複なし
+p g.uniq! #破壊的メソッド
+
+puts "\nテスト27"
+h = ["my", "name", "is", "kuzuba"]
+p h.join 
+p h.join(" ")
+p h.join("_")
+
+puts "\nテスト28"
+p h.sort
+p h.sort.reverse
+p h.size
+
+puts "\nテスト29"
+puts "\nテスト30"
+puts "\nテスト31"
+
+
